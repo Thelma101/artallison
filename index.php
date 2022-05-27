ec3510f04354e0b3d911a223581b91e12501f346
Thelma Akpata <50399915+Thelma101@users.noreply.github.com>
2022-05-27 15:14:48 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,237 @@
+<?php include 'header.php'; 
+ ?>
+ 
+    <!-- Slider -->
+    <div id="home">
+        <div class="dorothea-hero js-fullheight">
+            <div class="flexslider js-fullheight">
+                <ul class="slides">
+                    <li class="bg-fixed" style="background-image: url(images/cAllison1.jpeg);">
+                        <div class="overlay"></div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12 js-fullheight slider-text">
+                                    <div class="slider-text-inner">
+                                        <div class="mx-auto frame-inner">
+                                            <h2>Art <mark> Allison</mark></h2>
+                                            <h6><mark>Artist </mark></h6> 
+                                            <span class="frame-1"></span> 
+                                            <span class="frame-2"></span> 
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="bg-fixed" style="background-image: url(images/artallison_with_brushes.png);">
+                        <div class="overlay"></div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12 js-fullheight slider-text">
+                                    <div class="slider-text-inner">
+                                        <div class="mx-auto frame-inner">
+                                            <h2>Art <mark>Allison</mark></h2>
+                                            <h6><mark>Artist</mark></h6> 
+                                            <span class="frame-1"></span> 
+                                            <span class="frame-2"></span> 
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="bottom-fade"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="number"><a href="+2348173998902" target="_blank"><span>+234 817 399 8902</span></a> </div>
+                        <div class="arrow bounce text-center">
+                            <a href="#about" class=""> <i class="ti-angle-double-down"></i> </a>
+                        </div>
+                        <div class="right-bottom-text"><span>Christian Allison / Artist</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Services -->
+<!--    <div id="services" class="section-padding services">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center"> <span class="dorothea-heading-meta animate-box" data-animate-effect="fadeInUp">What I Do</span>
+                    <h2 class="dorothea-heading animate-box" data-animate-effect="fadeInUp">My Services</h2> </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 mtop-60 animate-box" data-animate-effect="fadeInUp">
+                    <div class="item">
+                        <div class="position-re o-hidden"> <img src="images/services/1.jpg" alt=""> </div>
+                        <div class="con"> <i class="ti-more"></i>
+                            <h5><a href="services-post.php">Wedding Photography</a></h5>
+                            <div class="line"></div> <a href="services-post.php"><i class="ti-arrow-right"></i></a> </div>
+                    </div>
+                </div>
+                <div class="col-md-3 animate-box" data-animate-effect="fadeInUp">
+                    <div class="item">
+                        <div class="position-re o-hidden"> <img src="images/services/2.jpg" alt=""> </div>
+                        <div class="con"> <i class="ti-more"></i>
+                            <h5><a href="services-post.php">Portrait Photography</a></h5>
+                            <div class="line"></div> <a href="services-post.php"><i class="ti-arrow-right"></i></a> </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mtop-60 animate-box" data-animate-effect="fadeInUp">
+                    <div class="item">
+                        <div class="position-re o-hidden"> <img src="images/services/3.jpg" alt=""> </div>
+                        <div class="con"> <i class="ti-more"></i>
+                            <h5><a href="services-post.php">Product Photography</a></h5>
+                            <div class="line"></div> <a href="services-post.php"><i class="ti-arrow-right"></i></a> </div>
+                    </div>
+                </div>
+                <div class="col-md-3 animate-box" data-animate-effect="fadeInUp">
+                    <div class="item">
+                        <div class="position-re o-hidden"> <img src="images/services/5.jpg" alt=""> </div>
+                        <div class="con"> <i class="ti-more"></i>
+                            <h5><a href="services-post.php">Sporty Photography</a></h5>
+                            <div class="line"></div> <a href="services-post.php"><i class="ti-arrow-right"></i></a> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-right"> <a class="underline-text" href="services.php">All Services <i class="ti-arrow-right"></i></a> </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- About -->
+    <div id="about" class="about-banner-section about">
+        <div class="background-image" style="background: url('images/banner.jpg') center center; background-size: cover;"> </div>
+        
+        <div class="bottom-fade"> </div>
+        <div class="wrap-text"> 
+            <span class="heading-meta"> <p>Hyper Realistic Portrait Artist</p></span>
+            <h2>Christian Allison</h2>
+            <p>I am inspired by the events that happen around me, in my community, in my country and in the world. I am an introvert who found a voice through art.
+            
+            <br><br>I am a two dimensional Hyper Realistic Portriat Artist with several years of experience. I make drawings on canvas or paper using Chalk or oil pastel, Charcoal and graphite which is my preferred medium. 
+            
+            </p> 
+       <!--    <img class="signature" src="images/sign-gray.png" alt=""> 
+       <div></div> -->
+       <video width="600" height="350" controls>
+        <source src="images/Christian Allison - African identity through hyperrealism.mp4" type="video/mp4">
+        </video>
+
+                            <h6>Christian Allison - African identity through hyperrealism</h6>
+                        </div>
+    </div>
+ </div>
+    
+    
+    <!-- Gallery -->
+    <div class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center"> 
+                    <span class="dorothea-heading-meta animate-box" data-animate-effect="fadeInUp">Portfolio</span>
+                    <h2 class="dorothea-heading animate-box" data-animate-effect="fadeInUp">Gallery</h2> 
+                </div>
+            </div>
+            <div class="row dorothea-photos">
+                <div class="col-12 col-md-4 animate-box" data-animate-effect="fadeInUp">
+                    <a class="d-block dorothea-photo-item" data-fancybox="images" data-caption="Photography" href="images/gallery/artAllison (1).jpeg"> <img class="img-fluid" src="images/gallery/artAllison (1).jpeg" alt=""> </a>
+                    <a class="d-block dorothea-photo-item" data-fancybox="images" data-caption="Photography" href="images/gallery/artAllison (2).jpeg"> <img class="img-fluid" src="images/gallery/artAllison (2).jpeg" alt=""> </a>
+                </div>
+                <div class="col-12 col-md-4 animate-box" data-animate-effect="fadeInUp">
+                    <a class="d-block dorothea-photo-item" data-fancybox="images" data-caption="Photography" href="images/gallery/artAllison (5).jpeg"> <img class="img-fluid" src="images/gallery/artAllison (5).jpeg" alt=""> </a>
+                    <a class="d-block dorothea-photo-item" data-fancybox="images" data-caption="Photography" href="images/gallery/artAllison (4).jpeg"> <img class="img-fluid" src="images/gallery/artAllison (4).jpeg" alt=""> </a>
+                    <a class="d-block dorothea-photo-item" data-fancybox="images" data-caption="Photography" href="images/gallery/"> <img class="img-fluid" src="images/gallery/" alt=""> </a>
+                </div>
+                <div class="col-12 col-md-4 animate-box" data-animate-effect="fadeInUp">
+                    <a class="d-block dorothea-photo-item" data-fancybox="images" data-caption="Photography" href="images/gallery/artAllison_Gallery (20).jpg"> <img class="img-fluid" src="images/gallery/artAllison_Gallery (20).jpg" alt=""> </a>
+                    <a class="d-block dorothea-photo-item" data-fancybox="images" data-caption="Photography" href="images/gallery/artAllison_Gallery (8).jpg"> <img class="img-fluid" src="images/gallery/artAllison_Gallery (8).jpg" alt=""> </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-right"> <a class="underline-text" href="gallery.php">All Gallery <i class="ti-arrow-right"></i></a> </div>
+            </div>
+        </div>
+    </div>
+    <!-- Blog -->
+    <div class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <span class="dorothea-heading-meta animate-box" data-animate-effect="fadeInUp">Read</span>
+                    <h2 class="dorothea-heading animate-box" data-animate-effect="fadeInUp">Poetry &amp; Blog</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
+                    <div class="blog-entry">
+                        <a href="post.php" class="blog-img"><img src="images/blog/" class="img-fluid" alt=""></a>
+                        <div class="desc">
+                            <span>Sep 25, 2021 | Poetry</span>
+                            <h3><a href="post.php">BEYOND BEAUTY</a></h3>
+                            <p>Beauty is in the eyes of the<br> 
+                            beholder, they say, but whose eyes <br>
+                            shall behold mine? I ask <br>
+                                The patch on my skin reveals 
+                              </p>
+                            <a class="underline-text" href="post.php">Read more <i class="ti-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
+                    <div class="blog-entry">
+                        <a href="post.php" class="blog-img"><img src="images/blog/" class="img-fluid" alt=""></a>
+                        <div class="desc"> <span>Jan 08, 2022 | Poetry</span>
+                            <h3><a href="post.php">REFLECTIONS</a></h3>
+                            <p>As days go by, <br>
+                        One wonders.<br>
+                        How life is enjoyable<br>
+                        At the same time, fleeting<br>
+                        </p>
+                            <a class="underline-text" href="post.php">Read more <i class="ti-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 animate-box" data-animate-effect="fadeInUp">
+                    <div class="blog-entry">
+                        <a href="post.php" class="blog-img"><img src="images/blog/" class="img-fluid" alt=""></a>
+                        <div class="desc"> <span>Jan 22, 2022 | Poetry</span>
+                            <h3><a href="post.php"> BOUNDLESS </h3></a>
+                               <p> <br>Within her pain lies her victory
+                                        <br>From days of yore,
+                                        <br>For she was marred,
+                                        <br>For the colour of her skin.</p>
+                            <a class="underline-text" href="post.php">Read more <i class="ti-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<form action="action_page.php">
+  <div class="container" align= "center">
+     <div class="center" border = "3%">
+    <h2>Subscribe to our Newsletter</h2>
+    <p>Enjoy fresh and entertaining content</p>
+  </div>
+     <div class="container" style="background-color:#000" "color:#fff;">
+    <input type="text" style="background-color:#000" "color:#fff;" placeholder="Name" name="name" required>
+    <input type="text"style="background-color:#000" "color:#fff;"  placeholder="Email address" name="mail" required "color:black";>
+    <label>
+      <input type="checkbox" checked="checked" name="subscribe"> Accept our Terms and Conditions
+    </label>
+  </div>
+  </div>
+ </div>
+ 
+ 
+ <?php include 'footer.php'; 
+ ?>
